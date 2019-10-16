60e544f75eb7784dbf57c3622d09a7285419336b
Sohang Chopra <sohangchopra@gmail.com>
2019-10-16 19:58:02 +0530

    Removed usage of Unit class because of excessive complexity

--- client/js/PointCharge.js
+++ client/js/PointCharge.js
@@ -1,22 +1,22 @@
-import {Unit} from './Unit.js'
 import {drawCircle} from './drawShapes.js'
 
-const DEFAULT_LENGTH_UNIT  = 'pixel';
-
-const POINT_RADIUS         = new Unit(5, DEFAULT_LENGTH_UNIT);        
+const POINT_RADIUS         = 5;        
 const PLUS_CHARGE_COLOR    = 'red';
 const MINUS_CHARGE_COLOR   = 'skyblue';
 const NEUTRAL_CHARGE_COLOR = 'white';
 
 /** Represents an electrical point charge */
 export class PointCharge {
+    /** @param {Object} info - contains fields mass, charge, position, velocity
+      * (Default velocity is 0)
+      */
     constructor(info) {
         this.mass     = info.mass;
         this.charge   = info.charge;
-        this.pos      = info.pos;
-        this.velocity = info.velocity;
+        this.position = info.pos;
+        this.velocity = info.velocity || 0;
         
-        const charge = this.charge.as('coulumb');
+        const charge = this.charge;
         Object.defineProperties(this, {
             radius: {value: POINT_RADIUS},
             color:  {value: charge >= 0 ? charge == 0 ? NEUTRAL_CHARGE_COLOR : MINUS_CHARGE_COLOR : PLUS_CHARGE_COLOR}
@@ -25,12 +25,12 @@ export class PointCharge {
 
     /** @param {CanvasRenderingContext2d} context */
     render(context) {               // drawing context, obtained from canvas
-        drawCircle(context, this.pos.as(DEFAULT_LENGTH_UNIT), this.radius.as(DEFAULT_LENGTH_UNIT), {
+        drawCircle(context, this.position, this.radius, {
             fill:   {color: this.color},
             stroke: null
         });
     }
 
-    accelerate
+   
 }
 
